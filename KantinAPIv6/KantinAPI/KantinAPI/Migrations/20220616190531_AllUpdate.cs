9b6b133d36540fe374967974ee579a870402cbe0
abdullahberbeer <104263146+abdullahberbeer@users.noreply.github.com>
2022-06-26 04:44:00 +0300

    KantinAPIv6
    OrderCompletted

---
+++ KantinAPIv6/KantinAPI/KantinAPI/Migrations/20220616190531_AllUpdate.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KantinAPI.Migrations
+{
+    public partial class AllUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Baskets_AspNetUsers_UserId",
+                table: "Baskets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "PersonId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                newName: "IX_Orders_PersonId");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Baskets",
+                newName: "PersonId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Baskets_UserId",
+                table: "Baskets",
+                newName: "IX_Baskets_PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Baskets_Persons_PersonId",
+                table: "Baskets",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Persons_PersonId",
+                table: "Orders",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Baskets_Persons_PersonId",
+                table: "Baskets");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Persons_PersonId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonId",
+                table: "Orders",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_PersonId",
+                table: "Orders",
+                newName: "IX_Orders_UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonId",
+                table: "Baskets",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Baskets_PersonId",
+                table: "Baskets",
+                newName: "IX_Baskets_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Baskets_AspNetUsers_UserId",
+                table: "Baskets",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ KantinAPIv6/KantinAPI/KantinAPI/Migrations/20220625220624_AddOrderItemsPersonId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KantinAPI.Migrations
+{
+    public partial class AddOrderItemsPersonId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PersonId",
+                table: "OrderItems",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                table: "OrderItems");
+        }
+    }
+}

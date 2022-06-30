775035f3e22a400c2f9e3aa89e9ebc7c70360795
abdullahberbeer <104263146+abdullahberbeer@users.noreply.github.com>
2022-06-30 05:05:20 +0300

    KantinAPIv7
    Completted1

---
+++ KantinAPIv7/KantinAPI/KantinAPI/Migrations/20220616203644_UpdatOrder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KantinAPI.Migrations
+{
+    public partial class UpdatOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Orders",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Orders");
+        }
+    }
+}

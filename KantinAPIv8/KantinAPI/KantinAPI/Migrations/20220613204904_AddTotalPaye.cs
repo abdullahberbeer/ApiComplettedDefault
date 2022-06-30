1eeb5ed49b5b08d9e3b6e5865af42aede704f9fd
abdullahberbeer <104263146+abdullahberbeer@users.noreply.github.com>
2022-06-30 17:01:57 +0300

    KantinAPIv8
    Completted2

---
+++ KantinAPIv8/KantinAPI/KantinAPI/Migrations/20220613204904_AddTotalPaye.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KantinAPI.Migrations
+{
+    public partial class AddTotalPaye : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "TotalPaye",
+                table: "Baskets",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPaye",
+                table: "Baskets");
+        }
+    }
+}
